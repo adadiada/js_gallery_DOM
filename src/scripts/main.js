c67bac99932c4d91daed2ca11a23f54731dd6e49
Hanna Yatskiv <hanna.ada7@gmail.com>
2025-11-05 08:21:31 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', () => {
+  const link = event.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+  event.preventDefault();
+
+  const bigSrc = link.href || link.dataset.preview;
+
+  largeImg.src = bigSrc;
+});
