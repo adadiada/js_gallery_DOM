a1f44c22762e0246491fdeb1df101581d4ad482e
Hanna Yatskiv <hanna.ada7@gmail.com>
2025-11-05 08:39:13 +0300

    make build

---
+++ index.e0257f1e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),r=document.querySelector("#largeImg");e.addEventListener("click",function(e){var t=e.target.closest("a");if(t){e.preventDefault();var a=t.href||t.dataset.preview;r.src=a}});
+//# sourceMappingURL=index.e0257f1e.js.map
